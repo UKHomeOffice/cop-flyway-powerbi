601432996bb9dfe1904a7442c3f743c9e04c593f
Belinda Cowey <belinda.cowey@digital.homeoffice.gov.uk>
2020-04-06 17:14:25 +0100

    DATA-227 Flwyay SQL Server Powerbi configuration

--- docker/run.sh
+++ docker/run.sh
@@ -5,82 +5,45 @@ set +x
 
 echo "Exporting environment variables"
 
-export DB_POWERBI_DEFAULT_DBNAME=${DB_POWERBI_DEFAULT_DBNAME}
-export DB_POWERBI_HOSTNAME=${DB_POWERBI_HOSTNAME}
-export DB_POWERBI_PORT=${DB_POWERBI_PORT}
-export DB_POWERBI_OPTIONS=${DB_POWERBI_OPTIONS}
-export DB_POWERBI_JDBC_OPTIONS=${DB_POWERBI_JDBC_OPTIONS}
-export URL="postgresql://${DB_POWERBI_HOSTNAME}:${DB_POWERBI_PORT}/${DB_POWERBI_DEFAULT_DBNAME}${DB_POWERBI_OPTIONS}"
-export FLYWAY_URL="jdbc:"postgresql://${DB_POWERBI_HOSTNAME}:${DB_POWERBI_PORT}/${DB_POWERBI_DEFAULT_DBNAME}${DB_POWERBI_JDBC_OPTIONS}""
-export DB_POWERBI_DEFAULT_USERNAME=${DB_POWERBI_DEFAULT_USERNAME}
-export DB_POWERBI_DEFAULT_PASSWORD=${DB_POWERBI_DEFAULT_PASSWORD}
-export FLYWAY_PLACEHOLDERS_MASTERUSER=${DB_POWERBI_DEFAULT_USERNAME}
-export FLYWAY_PLACEHOLDERS_POWERBIOWNERNAME=${DB_POWERBI_POWERBI_OWNER_USERNAME}
-export FLYWAY_PLACEHOLDERS_POWERBIOWNERPASSWORD=${DB_POWERBI_POWERBI_OWNER_PASSWORD}
-export FLYWAY_PLACEHOLDERS_POWERBISCHEMA=${DB_POWERBI_POWERBI_SCHEMA}
-export PGPASSWORD=${DB_POWERBI_DEFAULT_PASSWORD}
+# jdbc:sqlserver://<host>:<port>;databaseName=<database>
+export FLYWAY_URL="jdbc:sqlserver://${DB_POWERBI_HOSTNAME}:${DB_POWERBI_PORT};databaseName=${DB_POWERBI_DBNAME}"
+export FLYWAY_BASELINE_ON_MIGRATE="true"
 
 export BASEPATH="${PWD}"
 echo "Running from base path: ${BASEPATH}"
 
-echo "Checking if postgres is up and ready for connections"
+echo "Checking if SQL server is up and ready for connections"
 i=0
-pg_isready -d ${URL} -U ${DB_POWERBI_DEFAULT_USERNAME} -t 60
-PG_EXIT=$?
-while [[ "${i}" -lt "5" && ${PG_EXIT} != 0 ]]
+/opt/mssql-tools/bin/sqlcmd -S ${DB_POWERBI_HOSTNAME} -U ${DB_POWERBI_FLYWAY_USERNAME} -P ${DB_POWERBI_FLYWAY_PASSWORD} -d ${DB_POWERBI_DBNAME} -Q "SELECT DATABASEPROPERTYEX(N'database name', 'Collation')" -t 5
+SS_EXIT=$?
+while [[ "${i}" -lt "5" && ${SS_EXIT} != 0 ]]
 do
-    echo "waiting for Postgres to start, attempt: ${i}"
-    sleep 5s
+    echo "waiting for SQL Server to start, attempt: ${i}"
+    sleep 15s
     if [[ "${i}" > 5 ]]
     then
-        echo "Error: failed waiting for Postgres to start"
+        echo "Error: failed waiting for SQL Server to start"
         exit 1
     fi
     ((i++))
-    pg_isready -d ${URL} -U ${DB_POWERBI_DEFAULT_USERNAME} -t 60
-    PG_EXIT=$?
+    /opt/mssql-tools/bin/sqlcmd -S ${DB_POWERBI_HOSTNAME} -U ${DB_POWERBI_FLYWAY_USERNAME} -P ${DB_POWERBI_FLYWAY_PASSWORD} -d ${DB_POWERBI_DBNAME} -Q "SELECT DATABASEPROPERTYEX(N'database name', 'Collation')" -t 5
+    SS_EXIT=$?
 done
 
-echo "Creating initial databases"
-export FLYWAY_USER=${DB_POWERBI_DEFAULT_USERNAME}
-export FLYWAY_PASSWORD=${DB_POWERBI_DEFAULT_PASSWORD}
-
-
-echo "Checking if database exists"
-STATUS=$( psql postgresql://${DB_POWERBI_DEFAULT_USERNAME}@${DB_POWERBI_HOSTNAME}:${DB_POWERBI_PORT}/${DB_POWERBI_DEFAULT_DBNAME}${DB_POWERBI_OPTIONS} -tc "SELECT 1 FROM pg_database WHERE datname='${DB_POWERBI_POWERBIERENCE_DBNAME}'" | sed -e 's/^[ \t]*//')
-if [[ "${STATUS}" == "1" ]]
-then
-    echo "Database already exists"
-else
-    echo "Database does not exist creating - Bootstrapping databases"
-    export FLYWAY_LOCATIONS="filesystem:${BASEPATH}/schemas/init"
-    flyway -configFiles=${BASEPATH}/docker/flyway_init_docker.conf migrate
-    if [[ "$?" != 0 ]]
-    then
-        echo "Error: initialising database"
-        exit 1
-    fi
-    cd ${BASEPATH}/docker/
-    yasha bootstrap.j2 -o /tmp/bootstrap.sql
-    psql postgresql://${DB_POWERBI_DEFAULT_USERNAME}@${DB_POWERBI_HOSTNAME}:${DB_POWERBI_PORT}/${DB_POWERBI_DEFAULT_DBNAME}${DB_POWERBI_OPTIONS} < /tmp/bootstrap.sql
-    if [[ "$?" != 0 ]]
-    then
-        echo "Error: with bootstrapping database"
-        exit 1
-    fi
-fi
-
 echo "Migrating powerbi database"
-export FLYWAY_URL="jdbc:postgresql://${DB_POWERBI_HOSTNAME}:${DB_POWERBI_PORT}/${DB_POWERBI_POWERBIERENCE_DBNAME}${DB_POWERBI_JDBC_OPTIONS}"
-export FLYWAY_USER=${DB_POWERBI_POWERBI_OWNER_USERNAME}
-export FLYWAY_PASSWORD=${DB_POWERBI_POWERBI_OWNER_PASSWORD}
-export FLYWAY_SCHEMAS=${DB_POWERBI_POWERBI_SCHEMA}
-export FLYWAY_PLACEHOLDERS_SCHEMA=${DB_POWERBI_POWERBI_SCHEMA}
-export FLYWAY_PLACEHOLDERS_AUTHENTICATORUSER=${DB_POWERBI_POWERBI_AUTHENTICATOR_USERNAME}
-export FLYWAY_PLACEHOLDERS_AUTHENTICATORPASSWORD=${DB_POWERBI_POWERBI_AUTHENTICATOR_PASSWORD}
-export FLYWAY_PLACEHOLDERS_ANONUSER=${DB_POWERBI_POWERBI_ANON_USERNAME}
-export FLYWAY_PLACEHOLDERS_SERVICEUSER=${DB_POWERBI_POWERBI_SERVICE_USERNAME}
-export FLYWAY_PLACEHOLDERS_READONLYUSER=${DB_POWERBI_POWERBI_READONLY_USERNAME}
+ 
+export FLYWAY_USER=${DB_POWERBI_FLYWAY_USERNAME}
+export FLYWAY_PASSWORD=${DB_POWERBI_FLYWAY_PASSWORD}
+
+export FLYWAY_PLACEHOLDERS_DATABRICKSPASSWORD=${DB_POWERBI_DATABRICKS_PASSWORD}
+export FLYWAY_PLACEHOLDERS_DATABRICKSUSERNAME=${DB_POWERBI_DATABRICKS_USERNAME}
+export FLYWAY_PLACEHOLDERS_DBNAME=${DB_POWERBI_DBNAME}
+export FLYWAY_PLACEHOLDERS_MASTERUSER=${DB_POWERBI_FLYWAY_USERNAME}
+export FLYWAY_PLACEHOLDERS_POWERBIPASSWORD=${DB_POWERBI_POWERBI_PASSWORD}
+export FLYWAY_PLACEHOLDERS_POWERBIUSERNAME=${DB_POWERBI_POWERBI_USERNAME}
+
+export SSPASSWORD=${DB_POWERBI_FLYWAY_PASSWORD}
+
 export FLYWAY_LOCATIONS="filesystem:${BASEPATH}/schemas/powerbi"
 
 flyway -configFiles=${BASEPATH}/docker/flyway_powerbi_docker.conf migrate
--- schemas/powerbi/V2__users.sql
+++ schemas/powerbi/V2__users.sql
@@ -1,6 +1,7 @@
-CREATE USER ${powerbiusername} WITH PASSWORD '${powerbipassword}';
+USE ${dbname};
+CREATE USER ${powerbiusername} WITH PASSWORD = '${powerbipassword}';
 ALTER ROLE db_datareader ADD MEMBER ${powerbiusername};
-CREATE USER ${databricksusername} WITH PASSWORD '${databrickspassword}'
+CREATE USER ${databricksusername} WITH PASSWORD = '${databrickspassword}';
 ALTER ROLE db_datareader ADD MEMBER ${databricksusername};
 ALTER ROLE db_datawriter ADD MEMBER ${databricksusername};
 ALTER ROLE db_ddladmin ADD MEMBER ${databricksusername};

df7092b12958872fe2b01020ed369be54edfcf74
Belinda Cowey <belinda.cowey@digital.homeoffice.gov.uk>
2020-04-14 10:34:29 +0100

    WIP

--- schemas/powerbi/V1__bootstrap.sql
+++ schemas/powerbi/V1__bootstrap.sql
@@ -1,12 +1,7 @@
 -- Setup DB
-CREATE ROLE ${authenticatoruser} WITH NOINHERIT LOGIN ENCRYPTED PASSWORD '${authenticatorpassword}';
-GRANT USAGE ON SCHEMA ${schema} TO ${authenticatoruser};
-CREATE ROLE ${anonuser} NOLOGIN;
-GRANT USAGE ON SCHEMA ${schema} TO ${anonuser};
-GRANT ${anonuser} to ${authenticatoruser};
-CREATE ROLE ${serviceuser} NOLOGIN;
-GRANT USAGE ON SCHEMA ${schema} TO ${serviceuser};
-GRANT ${serviceuser} to ${authenticatoruser};
-CREATE ROLE ${readonlyuser} NOLOGIN;
-GRANT USAGE ON SCHEMA ${schema} TO ${readonlyuser};
-GRANT ${readonlyuser} to ${authenticatoruser};
+CREATE ROLE ${databricksuser} WITH NOINHERIT LOGIN ENCRYPTED PASSWORD '${databrickspassword}';
+GRANT USAGE ON SCHEMA ${schema} TO ${databricksuser};
+GRANT ${databricksuser} to ${databricksuser};
+CREATE ROLE ${powerbiuser} WITH NOINHERIT LOGIN ENCRYPTED PASSWORD '${powerbipassword}';
+GRANT USAGE ON SCHEMA ${schema} TO ${powerbiuser};
+GRANT ${powerbiuser} to ${powerbiuser};

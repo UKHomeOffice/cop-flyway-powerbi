114985c07920d687bea899de3701c5ab12cb71c9
Matthew Bradley <34741642+branchfork@users.noreply.github.com>
2020-04-15 11:37:32 +0100

    Update V1__reference.sql

--- schemas/init/V1__reference.sql
+++ schemas/init/V1__reference.sql
@@ -1,3 +1,6 @@
-CREATE ROLE ${powerbiownername} WITH CREATEROLE LOGIN ENCRYPTED PASSWORD '${powerbiownerpassword}';
-GRANT ${powerbiownername} TO ${masteruser};
-CREATE DATABASE ${powerbidbname} OWNER ${powerbiownername};
+CREATE USER ${powerbiusername} WITH PASSWORD '${powerbipassword}';
+ALTER ROLE db_datareader ADD MEMBER ${powerbiusername};
+CREATE USER ${databricksusername} WITH PASSWORD '${databrickspassword}'
+ALTER ROLE db_datareader ADD MEMBER ${databricksusername};
+ALTER ROLE db_datawriter ADD MEMBER ${databricksusername};
+ALTER ROLE db_ddladmin ADD MEMBER ${databricksusername};
